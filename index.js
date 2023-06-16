3b8c9df7d8e723929f2689ae8685d58bd74ae72f
Morrie T Bradley <morrietbradley116@gmail.com>
2023-06-16 14:14:31 -0700

    WIP add slide controls
    Co-authored-by: NariaNexis <nnanimefreak5@gmail.com>

--- index.js
+++ index.js
@@ -27,10 +27,14 @@ window.onload = function () {
     const coolsound = document.getElementById("firstsound");
     const myAudio = document.getElementById("myAudio")
 
+    const prevSlide = document.getElementById("prevSlide");
+
+
     let i = 0
     coolest.setAttribute("src", database[i].img)
 
     coolest.onclick = function () {
+        console.log(i)
         myAudio.load()
         myAudio.play();
         i++
@@ -44,4 +48,13 @@ window.onload = function () {
         }
     }
 
+    prevSlide.onclick = function () {
+        console.log("i=" + i)
+        let h = i - 1
+        coolest.setAttribute("src", database[h].img)
+        coolsound.setAttribute("src", database[h].sound)
+        myAudio.load()
+        myAudio.play();
+        console.log("h=" + h)
+    }
 };
